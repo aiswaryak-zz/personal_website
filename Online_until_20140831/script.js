922f33932c20ad9f4ceec31c7556c3823e7357dc
Aiswarya Kolisetty <akoliset@ford.com>
2014-12-19 11:06:48 -0500

    first commit of website

---
+++ Online_until_20140831/script.js
@@ -0,0 +1,15 @@
+  $(function() {
+    $( "#accordion" ).accordion();
+
+    $(function() {
+    $( "#tabs" ).tabs();
+  });
+
+$(document).ready(function() {
+        $("#tabs").tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
+        $("#tabs li").removeClass('ui-corner-top').addClass('ui-corner-left');
+
+    });
+
+  });
+
